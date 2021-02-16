25ab13c14cc40882859b8f18c79d9c35cfa4b75b
Allyson <allysoneduardo04@gmail.com>
2021-02-15 22:56:29 -0300

    feat: added updateBalance()

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -72,6 +72,12 @@ const DOM = {
         `
 
         return html
+    },
+
+    updateBalance() {
+        document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(Transaction.incomes())
+        document.getElementById("expenseDisplay").innerHTML = Utils.formatCurrency(Transaction.expenses())
+        document.getElementById("totalDisplay").innerHTML = Utils.formatCurrency(Transaction.total())
     }
 }
 
